2a112acd6fde209861570f9baccb92014f9fa074
Grant Makyan <grant.makyan@gmail.com>
2024-01-02 22:32:37 +0300

    Add some comments

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ import os
 
 import pyrogram
 
+# Envs
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 TARGET_CHAT_ID = int(os.environ.get("TARGET_CHAT_ID"))
 FORWARD_CHAT_ID = int(os.environ.get("FORWARD_CHAT_ID"))
 
+# Initialize Telegram client
 client = pyrogram.Client(
     "account",
     api_id=API_ID,
@@ -35,5 +37,6 @@ async def message_handler(_, message: pyrogram.types.Message):
     await message.forward(FORWARD_CHAT_ID)
 
 
+# Start client
 if __name__ == "__main__":
     client.run()
